92aeb73b93bfbde78405ed98cb2f16469b155db0
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-01-07 17:48:54 -0500

    reintroduce a better routing engine

---
+++ lib/monet/router.rb
@@ -0,0 +1,66 @@
+module Monet
+  class Router
+    include URLHelpers
+    TYPES = [
+      :baseline,
+      :capture,
+      :thumbnail
+    ]
+
+    def initialize(config)
+      @config = config
+    end
+
+    TYPES.each do |type|
+      define_method "#{type}_dir" do |filename|
+        File.join @config.send("#{type}_dir"), @config.site, filename
+      end
+
+      define_method "#{type}_url" do |filename|
+        type_dir = @config.send "#{type}_dir"
+        image_url(type_dir, filename)
+      end
+    end
+
+    def original_url(path)
+      url = path.split("/").last
+      path = url.split('|')[1..-1].join("/").gsub(/-\d+\.png/, "")
+
+      "#{@config.base_url}/#{path}"
+    end
+
+    def diff_path(path)
+      diff_name = basename(path).gsub(".png", "-diff.png")
+      File.join @config.baseline_dir, @config.site, diff_name
+    end
+
+    def capture_routes
+      urls = {}
+      @config.map.paths.each do |path|
+        url = "#{@config.base_url}/#{path}"
+        @config.dimensions.each do |width|
+          urls[url] ||= []
+          urls[url] << url_to_filepath(url, width)
+        end
+      end
+
+      urls
+    end
+
+    def url_to_filepath(url, width="*")
+      uri = parse_uri(url)
+      name = File.join @config.site, "#{@config.site}#{uri.path.gsub(/\//, '|')}"
+      File.join @config.capture_dir, "#{name}-#{width}.png"
+    end
+
+    private
+    def basename(path)
+      File.basename path
+    end
+
+    def image_url(type_dir, name)
+      relative_path = Pathname.new(type_dir).relative_path_from(Pathname.new(File.join(type_dir, "..")))
+      "/#{relative_path}/#{@config.root_dir}/#{name}"
+    end
+  end
+end
---
+++ spec/router_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+require 'monet/router'
+
+describe "Monet::Router" do
+  Given(:config) { Monet::Config.new({
+    base_url: "http://lance.com",
+    capture_dir: "./spec/fixtures/captures",
+    baseline_dir: "./spec/fixtures/baselines",
+    thumbnail_dir: "./spec/fixtures/thumbnails"
+  })}
+  Given(:router) { Monet::Router.new config }
+
+
+  def expand *parts
+    File.expand_path File.join(*parts)
+  end
+
+  shared_examples "type routing" do |type|
+    context "can get the #{type} for a path" do
+      When(:route) { router.send "#{type}_dir", "about-500.png" }
+      Then { route.should == expand(config.send("#{type}_dir"), "lance.com", "about-500.png") }
+    end
+
+    context "can get the #{type} url for a path" do
+      When(:route) { router.send "#{type}_url", "about-500.png" }
+      Then { route.should == "/#{type}s/#{config.site}/about-500.png" }
+    end
+
+    context "can get the diff for a path" do
+      When(:route) { router.diff_path expand(config.send("#{type}_dir"), "lance.com", "about-500.png") }
+      Then { route.should == expand(config.baseline_dir, "lance.com", "about-500-diff.png") }
+    end
+  end
+
+  context "can convert url path to capture path" do
+    When(:path) { router.url_to_filepath("http://lance.com/my-fake-url/chromist", 2000) }
+    Then { path.should == File.join(config.capture_dir, "lance.com", "lance.com|my-fake-url|chromist-2000.png") }
+  end
+
+  context "can list out urls" do
+    Given do
+      config.dimensions = [1440,900]
+      config.map = ['home/index', 'home/show']
+    end
+
+    When(:urls) { router.capture_routes }
+    Then { urls.should == {
+      "http://lance.com/home/index" => [
+        expand(config.capture_dir, "lance.com", "lance.com|home|index-1440.png"),
+        expand(config.capture_dir, "lance.com", "lance.com|home|index-900.png")
+      ],
+      "http://lance.com/home/show" => [
+        expand(config.capture_dir, "lance.com", "lance.com|home|show-1440.png"),
+        expand(config.capture_dir, "lance.com", "lance.com|home|show-900.png")
+      ]}
+    }
+  end
+
+  context "original url" do
+    When(:route) { router.original_url "lance.com/lance.com|about-500.png" }
+    Then { route.should == "http://lance.com/about" }
+  end
+
+  context "baseline" do
+    it_should_behave_like "type routing", "baseline"
+  end
+
+  context "capture" do
+    it_should_behave_like "type routing", "capture"
+  end
+
+  context "thumbnail" do
+    it_should_behave_like "type routing", "thumbnail"
+  end
+
+end
