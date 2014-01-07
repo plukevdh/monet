a6b29a9a1ed6356b6f5173ccd8f29efafe3406bf
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-01-07 17:54:00 -0500

    slim down what image is resposible for

--- lib/monet/image.rb
+++ lib/monet/image.rb
@@ -1,126 +1,50 @@
-  require 'monet/url_helpers'
+require 'chunky_png'
+require 'monet/config'
+require 'monet/url_helpers'
 
 module Monet
   class Image
-    class << self
-      include URLHelpers
-    end
+    include URLHelpers
 
-    NoDiffFound = Class.new(Exception)
-
-    def initialize(path, config)
+    def initialize(path)
       @path = File.expand_path path
-      @config = config
     end
 
-    def self.from_url(url, config, width)
-      @url = url
-      uri = parse_uri(url)
-      path = image_path(config.capture_dir, uri, width)
-      new path, config
-    end
-
-    def self.from_config(config)
-      images = []
-      config.map.paths.each do |path|
-        config.dimensions.each do |width|
-          url = "#{config.base_url}#{path}"
-          images << from_url(url, config, width)
-        end
-      end
-
-      images
-    end
-
-    def self.all_for_site(site, config)
-      files = Dir.glob(File.join(config.baseline_dir, site, "*.png"))
-      files.map do |path|
-        new(path, config)
-      end
-    end
-
-    def baseline?
-      @path.include? @config.baseline_dir
-    end
-
-    def flagged?
-      begin
-        diff
-        true
-      rescue NoDiffFound => e
-        false
-      end
+    def is_diff?
+      name.include? "-diff"
     end
 
     def width
       @width ||= File.basename(@path, ".png").split("-").last.to_i
     end
 
-    def baseline
-      File.join @config.baseline_dir, basename
-    end
-
-    def thumbnail
-      File.join @config.thumbnail_dir, basename
-    end
-
-    def capture
-      File.join @config.capture_dir, basename
-    end
-
-    def image_url(type_dir, public_folder)
-      relative_path = Pathname.new(type_dir).relative_path_from(Pathname.new(public_folder))
-      "/#{relative_path}/#{root_dir}/#{name}"
-    end
-
-    def url
-      @url ||= begin
-        url = @path.split("/").last
-        path = url.split('|')[1..-1].join("/").gsub(/-\d+\.png/, "")
-
-        "#{@config.base_url}/#{path}"
-      end
-    end
-
     def name
       @name ||= File.basename @path
-    end
-
-    %w(baseline capture thumbnail).each do |type|
-      define_method "#{type}_url" do |public_folder|
-        type_dir = @config.send "#{type}_dir"
-        image_url(type_dir, public_folder)
-      end
     end
 
     def basename
       @path.split(File::SEPARATOR)[-2..-1].join(File::SEPARATOR)
     end
 
-    def diff
-      @diff ||= baseline.gsub(".png", "-diff.png")
-      raise NoDiffFound, "No diff exists for #{basename}" unless File.exists?(@diff)
-      @diff
-    end
-
-    def host
-      @host ||= self.class.host(@config.base_url)
-    end
-    alias :root_dir :host
-
-    def self.host(url)
-      parse_uri(url).host
-    end
-
     def to_image
       ChunkyPNG::Image.from_file(@path)
     end
 
-    private
-    def self.image_path(base_dir, uri, width)
-      base = host(uri)
-      name = "#{base}#{uri.path}".gsub(/\//, '|')
-      "#{base_dir}/#{base}/#{name}-#{width}.png"
+    def thumbnail!(save_to=nil)
+      img = to_image
+      short_edge = [img.width, img.height].min
+
+      cropped = img.crop(0, 0, short_edge, short_edge)
+      resized = cropped.resize(200, 200)
+
+      if save_to.nil?
+        save_to = @path.gsub(".png", "-thumb.png")
+      else
+        save_to = File.join save_to, File.basename(@path)
+      end
+
+      resized.save save_to
+      save_to
     end
   end
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,80 +1,36 @@
 require 'spec_helper'
-require 'monet/config'
+require 'monet'
 require 'monet/image'
 
 describe Monet::Image do
-  Given(:config) { Monet::Config.new(compare_type: "ColorBlend", capture_dir: "./spec/fixtures/captures", baseline_dir: "./spec/fixtures/baselines", thumbnail_dir: "./spec/fixtures/thumbnails", base_url: "http://lance.com", map: ["/test1", "/test2"], dimensions: [1234, 5678]) }
+  Given(:img) { Monet::Image.new("./spec/fixtures/baselines/lance.com/lance.com-1024.png") }
 
-  context "with a flagged image" do
-    When(:img) { Monet::Image.new("./spec/fixtures/baselines/lance.com/lance.com-1024.png", config) }
+  context "knows width" do
+    Then { img.width.should == 1024 }
+  end
 
-    context "is this a baseline?" do
-      Then { img.should be_baseline }
+  context "can create an image" do
+    Then { img.to_image.should be_a(ChunkyPNG::Image) }
+  end
+
+  context "generates files" do
+    Given(:img) { Monet::Image.new("./spec/tmp/test.png") }
+
+    after(:all) do
+      tmp = Dir.glob("./spec/tmp/**/*-thumb.png")
+      tmp.each {|f| File.delete f }
     end
 
-    context "is this image flagged?" do
-      Then { img.should be_flagged }
+    context "can thumbnail self without a path" do
+      When(:path) { img.thumbnail! }
+      Then { path.should_not have_failed }
+      And { File.exists?(path).should be_true }
     end
 
-    context "knows how to get the thumbnail for itself" do
-      Then { img.thumbnail.should == File.expand_path("./spec/fixtures/thumbnails/lance.com/lance.com-1024.png") }
+    context "can thumbnail self with a path" do
+      When(:path) { img.thumbnail!("./spec/tmp/output/thumbnails") }
+      Then { path.should_not have_failed }
+      And { File.exists?(path).should be_true }
     end
-
-    context "knows how to get the diff for itself" do
-      Then { img.diff.should == File.expand_path("./spec/fixtures/baselines/lance.com/lance.com-1024-diff.png") }
-    end
-
-    context "knows how to get the captured image for itself" do
-      Then { img.capture.should == File.expand_path("./spec/fixtures/captures/lance.com/lance.com-1024.png") }
-    end
-
-    context "knows width" do
-      Then { img.width.should == 1024 }
-    end
-  end
-
-  context "can produce a set of image specs from the config" do
-    When(:images) { Monet::Image.from_config(config) }
-    Then { images.count.should == 4 }
-    And { images.first.name.should == "lance.com|test1-1234.png" }
-  end
-
-  context "can create from a url" do
-    When(:img) { Monet::Image.from_url("http://lance.com/aboutus", config, "1024") }
-    Then { img.baseline.should == File.expand_path("./spec/fixtures/baselines/lance.com/lance.com|aboutus-1024.png") }
-    And { img.url.should == "http://lance.com/aboutus" }
-  end
-
-  context "can get url from base path" do
-    When(:img) { Monet::Image.new("./spec/fixtures/baselines/lance.com/lance.com-1024.png", config) }
-    Then { img.url.should == "http://lance.com/" }
-  end
-
-  context "raises exception when no diff found" do
-    Given(:img) { Monet::Image.new("./spec/fixtures/baselines/lance.com/lance.com|aboutus-1024.png", config) }
-    When(:diff) { img.diff }
-    Then { diff.should have_failed(Monet::Image::NoDiffFound) }
-  end
-
-  shared_examples "url mapping" do |type|
-    Given(:public_path) { File.join(File.dirname(__FILE__), "fixtures") }
-
-    context "can get an image url for an image" do
-      Given(:img) { Monet::Image.new("./spec/fixtures/#{type}/lance.com/lance.com|aboutus-1024.png", config) }
-      When(:url) { img.send "#{type}_url", public_path }
-      Then { url.should == "/#{type}s/lance.com/lance.com|aboutus-1024.png" }
-    end
-  end
-
-  context "baseline" do
-    it_should_behave_like "url mapping", "baseline"
-  end
-
-  context "baseline" do
-    it_should_behave_like "url mapping", "capture"
-  end
-
-  context "baseline" do
-    it_should_behave_like "url mapping", "thumbnail"
   end
 end
