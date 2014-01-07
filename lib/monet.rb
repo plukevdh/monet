044f4a3a6096467cefc32b2b05d62b6e3d12e664
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-01-07 17:55:33 -0500

    drop lame module methods

--- lib/monet.rb
+++ lib/monet.rb
@@ -1,34 +1,5 @@
 require "monet/version"
 require 'monet/config'
-require "monet/capture"
-require "monet/compare"
-require "monet/baseline_control"
 
 module Monet
-  class << self
-    def clean(opts)
-      config = load_config(opts)
-      Dir.glob(File.join(config.baseline_dir, "**", "*.png")).each do |img|
-        File.delete img
-      end
-    end
-
-    def capture(opts)
-      agent = Monet::Capture.new(load_config(opts))
-      agent.capture_all
-    end
-
-    def compare(opts)
-      control = Monet::BaselineControl.new(opts)
-      control.run
-    end
-
-    def config(&block)
-      Monet::Config.config block
-    end
-
-    def load_config(options)
-      Monet::Config.build_config(options)
-    end
-  end
 end
